2c8fc53c246dffaafba16a6b28416e30e619a237
emiliogzz24 <emiliogzz24@gmail.com>
2018-09-19 23:55:19 -0500

    Added factories which help us mock objects, requests which host the request specs - testing related to requests and finally the controllers which will handle the requests we're expecting. Another important thing is the fact we've added a response handler and an exception handler to simplify the flow of data

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'items/index'
+  get 'todos/index'
+  resources :todos do
+    resources :items
+  end
 end
